ac8b96849d2450a0b0472f42e11dbc7c4b93dae8
Matheus Pereira <msmatheuspereira@gmail.com>
2020-09-29 21:36:02 -0300

    configurando o MongoDB

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const app = require('./app');
+const mongoose = require('mongoose');
 
 require('dotenv').config({path:'variables.env'});
+
+// Conexão ao Banco de Dados
+mongoose.connect(process.env.DATABASE, { useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.Promise = global.Promise;
+mongoose.connection.on('error', (error)=>{
+    console.error("ERRO: "+error.message);
+});
 
 app.set('port', process.env.PORT || 7777);
 
