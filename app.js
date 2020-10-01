d010df318028eb1eb6fd157832a248508bbd0ccc
Matheus Pereira <msmatheuspereira@gmail.com>
2020-10-01 18:06:18 -0300

    Entendento templates parte 2

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use('/', router);
 
 app.use(express.json());
 
-app.engine('mst', mustache());
+app.engine('mst', mustache(__dirname+'/views/portails', '.mst'));
 app.set('view engine', 'mst');
 app.set('views', __dirname + '/views');
 
--- routes/index.js
+++ routes/index.js
@@ -2,15 +2,9 @@ const express = require('express');
 
 const router = express.Router();
 router.get('/', (req, res)=>{
-    // http://localhost:7777/?nome=SeuNome&idade=15&mostrar=true
+
     let obj = {
-        'nome': req.query.nome,
-        'idade': req.query.idade,
-        'mostrar':req.query.mostrar,
-        'ingredientes':[
-            {nome:'Arroz', qt:'20g'},
-            {nome:'Macarrão', qt:'100g'}
-        ]
+        'nome': req.query.nome
     };
     res.render('home', obj);
 });
