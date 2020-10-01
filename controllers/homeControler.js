ca4f7baf6aa6ef033c25f7a843a0351e39a07fd5
Matheus Pereira <msmatheuspereira@gmail.com>
2020-10-01 19:08:59 -0300

    Middleware

--- controllers/homeControler.js
+++ controllers/homeControler.js
@@ -1,6 +1,13 @@
+exports.userMiddleware = (req, res, next)=>{
+    let info = {name:'Matheus', id:123};
+    req.userInfo = info;
+    next();
+}
+
 exports.index = (req, res)=>{
     let obj = {
-        'pageTitle': 'Titulo de teste'
+        'pageTitle': 'Titulo de teste',
+        userInfo: req.userInfo
     };
     res.render('home', obj);
 }
