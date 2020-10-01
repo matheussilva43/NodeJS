2cbca9029f4f8a9485ef5c16a63aa745b4d70ca4
Matheus Pereira <msmatheuspereira@gmail.com>
2020-10-01 17:48:15 -0300

    Entendendo templates parte 1

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,15 @@ const express = require('express');
 
 const router = express.Router();
 router.get('/', (req, res)=>{
+    // http://localhost:7777/?nome=SeuNome&idade=15&mostrar=true
     let obj = {
-        'nome': 'Matheus',
-        'idade': 50
+        'nome': req.query.nome,
+        'idade': req.query.idade,
+        'mostrar':req.query.mostrar,
+        'ingredientes':[
+            {nome:'Arroz', qt:'20g'},
+            {nome:'Macarrão', qt:'100g'}
+        ]
     };
     res.render('home', obj);
 });
