f4bdd823d50e1c7ed54454711912b6fdeb3b83cd
Matheus Pereira <msmatheuspereira@gmail.com>
2020-10-03 15:15:52 -0300

    Not found 404

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const mustache = require('mustache-express');
-// Rotas
 const router = require('./routes/index');
 const helpers = require('./helpers');
+const errorHendler = require('./handlers/errorHendler');
 
 
 // configurações
@@ -10,13 +10,14 @@ const app = express();
 
 app.use((req, res, next)=>{
     res.locals.h = helpers;
-    res.locals.teste = '123';
     next();
 });
 
 app.use(express.json());
 
 app.use('/', router);
+
+app.use(errorHendler.notFound);
 
 app.engine('mst', mustache(__dirname+'/views/portails', '.mst'));
 app.set('view engine', 'mst');
