c631034d0611e20e6fb2e930cb7673ef5565cb93
Matheus Pereira <msmatheuspereira@gmail.com>
2020-10-18 19:39:54 -0300

    Upload de imagens parte 1 e 2

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const homeController = require('../controllers/homeControler');
 const userController = require('../controllers/userController');
 const postController = require('../controllers/postController');
+const imageMiddleware = require('../middlewares/imageMiddleware');
 
 const router = express.Router();
 router.get('/', homeController.index);
@@ -9,7 +10,11 @@ router.get('/users/login', userController.login);
 router.get('/users/register', userController.register);
 
 router.get('/post/add', postController.add);
-router.post('/post/add', postController.addAction);
+router.post('/post/add',
+    imageMiddleware.upload,
+    imageMiddleware.resize,
+    postController.addAction
+);
 
 router.get('/post/:slug/edit', postController.edit);
 router.post('/post/:slug/edit', postController.editAction);
