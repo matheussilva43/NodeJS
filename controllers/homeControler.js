39dabcf1b251ac2050d86d2cc0b772922a271a3e
Matheus Pereira <msmatheuspereira@gmail.com>
2020-10-19 19:29:57 -0300

    Tags com MongoDB Aggregations parte 1,2 e 3

--- controllers/homeControler.js
+++ controllers/homeControler.js
@@ -4,8 +4,19 @@ const Post = mongoose.model('Post');
 exports.index = async (req, res)=>{
     let responseJson = {
         'pageTitle': 'Home',
-        posts:[]
+        posts:[],
+        tags: []
     };
+
+    responseJson.tag = req.query.t;
+
+    const tags = await Post.getTagsList();
+    for(let i in tags) {
+        if(tags[i]._id == responseJson.tag) {
+            tags[i].class = "selected";
+        }
+    }
+    responseJson.tags = tags;
 
     const posts = await Post.find();
     responseJson.posts = posts;
