346591b9b1545f951b35ee9974ff1236e4d217ba
Matheus Pereira <msmatheuspereira@gmail.com>
2020-10-19 20:38:31 -0300

    Fazendo o Login

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,6 +4,21 @@ exports.login = (req, res)=>{
     res.render('login');
 }
 
+exports.loginAction = (req, res)=>{
+    const auth = User.authenticate();
+
+    auth(req.body.email, req.body.password, (error, result)=>{
+        if(!result) {
+            req.flash('error', 'Seu e-mail e/ou senha estão errados!');
+            res.redirect('/users/login');
+            return;
+        }
+
+        req.flash('success', 'Você foi logado com sucesso!');
+        res.redirect('/');
+    });
+}
+
 exports.register = (req, res)=>{
     res.render('register');
 }
@@ -12,11 +27,11 @@ exports.registerAction = (req, res)=>{
     const newUser = new User(req.body);
     User.register(newUser, req.body.password, (error)=>{
         if(error){
-            console.log('Erro ao registrar:', error);
-            res.redirect('/');
+            req.flash('error', 'Ocorreu um erro, tente mais tarde.');
+            res.redirect('/users/register');
             return
         }
-
-        res.redirect('/');
+        req.flash('success', 'Registro efetuado com sucesso. Faça o login');
+        res.redirect('/users/login');
     });
 }
--- routes/index.js
+++ routes/index.js
@@ -9,6 +9,7 @@ router.get('/', homeController.index);
 router.get('/users/login', userController.login);
 router.get('/users/register', userController.register);
 router.post('/users/register', userController.registerAction);
+router.post('/users/login', userController.loginAction);
 
 router.get('/post/add', postController.add);
 router.post('/post/add',
